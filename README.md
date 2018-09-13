05476a034d566e75fafeaf205f29887d0f8469a7
Tech-A46 <tech-a46@Tech-a46s-MacBook-Air.local>
2018-09-13 18:26:57 +0100

    Completed tic_tac_toe game apart from game winning checks. So i guess its not completely complete

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Sparta_Tic_Tac_toe
+# Sparta Tic-Tac-Toe
+
+## Description
+The aim of this project is to create the game Tic-Tac-Toe using Javascript.
+
+## Languages used
+Javascript
+
+## How to Download
+1. if you do not have git installed follow this [guide](https://gist.github.com/derhuerst/1b15ff4652a867391f03)
+2. In your browser, navigate to this [page](https://github.com/DavidSIJames/Sparta_Tic_Tac_toe)
+3. Open your Terminal and navigate to where you want to clone the repo.
+4. Once there, enter the following command to clone the repo:
+
+	```terminal
+	git clone git@github.com:DavidSIJames/Sparta_Tic_Tac_toe.git
+	```
+5. once the repo has been cloned, cd into it using this command
+
+	```terminal
+	cd sparta_Tic_Tac_toe
+	```
+6. you are now in the repo
+
+##Challenges
+The main challenge i am facing right now is how to program the winning conditions. i have a relative understanding of how to win condition will operate, but i do not understand how to code it.
+##Takeaway
+this homework has definately helped me understand the scale of work that will be done during the game week. 
+
+I am frustrated that i couldn't figure out the logic behind the game, but i am hoping that i will understand in the coming days. 
--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+// object creation
   const game = new Object();
   game.divs = document.getElementsByTagName('td');
   game.clicked =[0,0,0,0,0,0,0,0,0];
-  game.mark = ['','','','','','','','',''];
-  game.outWin = []
-  game.inWin = []
+  game.markX= ['','','','','','','','',''];
+  game.markY= ['','','','','','','','',''];
+  game.outWin = [[0,3,6],[1,4,7],[2,5,6],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]];
   game.turn = true;
   game.target = "";
 
-
+  // Event Listener for button clicks
   for(i=0;i<game.divs.length;i++){
     game.divs[i].addEventListener('click',(e) =>{
+        // if statement that checks if panel has been clicked before
       if (!e.target.classList.contains('X') && !e.target.classList.contains('O')) {
         if (game.turn == true){
+          // adding X and X class to the panel
           e.target.innerHTML = "X";
           e.target.setAttribute('class','X');
           game.mark[i] = 'X';
           console.log(game.mark[i]);
           game.turn = !game.turn;
         } else {
+          // adding O and O class to the panel
           e.target.innerHTML = "O";
           e.target.setAttribute('class','O');
           game.mark[i] = 'O';
@@ -27,18 +30,19 @@ document.addEventListener('DOMContentLoaded', () => {
           game.turn = !game.turn;
         }
       }
-      for (var j = 0; j < game.mark.length; j++) {
-        for (var k = 0; k < array.length; k++) {
+      // for loop for the win checker
+      for (j = 0; j < game.mark.length; j++) {
+        for ( k = 0; k < .length; k++) {
 
         }
       }
     })
   }
+  // click Event for the reset button
   document.getElementById('reset').addEventListener('click',() =>{
     for ( i = 0; i < game.divs.length; i++) {
       game.divs[i].innerHTML ='';
       game.divs[i].setAttribute('class','');
-
     }
     game.turn = true;
   })
